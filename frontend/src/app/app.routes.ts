7e1399849703ff49c5221a6df98fdc3698a010f1
handrianmhel <handrianmhel@gmail.com>
2026-01-21 06:22:22 +0800

    implemented tailwindcss v4 setup, including the manual conversion of scss to css, including angular json file the zone.js polyfill after npm install zone.js  as well as create the angular page for testing API spreadsheet extract and done preliminary code development of uploading spreadsheet file

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AnalyzePageComponent } from './analyze-page/analyze-page'; // No .component, just the filename
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'analyze',
+        component: AnalyzePageComponent
+    },
+    {
+        path: '',
+        redirectTo: 'analyze',
+        pathMatch: 'full'
+    }
+];
